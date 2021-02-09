0dc3eb776c325e0a34cf07867d69ed7a58666c9d
Maximilian Schulke <maximilian.schulke@moccu.com>
2021-02-09 14:45:47 +0100

    refactor the tests to work with new deps

--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
 const
 	assert = require('assert'),
 	chalk = require('chalk'),
-	eslint = require('eslint'),
-	report = new eslint.CLIEngine().executeOnFiles(['**/*.js']),
+	{ESLint} = require('eslint'),
+	eslint = new ESLint(),
 
 	lib = require('../dist/lib')
 ;
 
-assert.deepEqual(
+assert.deepStrictEqual(
 	Object.keys(lib),
 	[
-		'init',
 		'ignoreErrors',
-		'ignoreUrls'
+		'ignoreUrls',
+		'init'
 	],
 	'expose expected settings'
 );
@@ -27,16 +27,22 @@ assert.ok(
 	'ignore errors are available'
 );
 
-
 // Eslint:
-eslint.CLIEngine.getErrorResults(report.results).forEach((error) =>
-	error.messages.forEach((message) =>
-		global.console.error(
-			chalk.red(message.message),
-			chalk.white(`(${message.ruleId})`),
-			chalk.white(`\n\t${error.filePath}:${message.line}:${message.column}`)
-		)
-	)
-);
-assert.equal(report.errorCount, 0);
-assert.equal(report.warningCount, 0);
+eslint.lintFiles(['**/*.js'])
+	.then(reports => {
+		ESLint.getErrorResults(reports).forEach((error) =>
+			error.messages.forEach((message) =>
+				global.console.error(
+					chalk.red(message.message),
+					chalk.white(`(${message.ruleId})`),
+					chalk.white(`\n\t${error.filePath}:${message.line}:${message.column}`)
+				)
+			)
+		);
+
+		reports.forEach((report) => {
+			assert.strictEqual(report.errorCount, 0);
+			assert.strictEqual(report.warningCount, 0);
+		});
+	})
+	.catch(e => console.error(`Failed to run eslint (${e})`));
